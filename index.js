79f64a8ad175c873e252f853825abad448a57aad
ibjohansen <ib.johansen@inmeta.com>
2015-04-28 00:00:50 +0200

    Added domSafeRandomGuid, a function that returns an id tested unique aagainst the document

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 "use strict";
 
 function randomGuid(numberOfBlocks) {
-	num = numberOfBlocks || 4;
+    num = numberOfBlocks || 4;
 
-	function s4() {
-		return Math.floor((1 + Math.random()) * 0x10000)
-			.toString(16)
-			.substring(1);
-	}
-	var output = '';
-	var num = numberOfBlocks;
-	while (num > 0) {
-		output += s4();
-		if (num > 1) output += "-";
-		num--;
-	}
-	return output;
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    var output = '';
+    var num = numberOfBlocks;
+    while (num > 0) {
+        output += s4();
+        if (num > 1) output += "-";
+        num--;
+    }
+    return output;
 }
 module.exports = randomGuid;
+
+//generate a guid that is tested unique against id's on the current doc
+function domSafeRandomGuid(numberOfBlocks) {
+    num = numberOfBlocks || 4;
+
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+
+    var output = '';
+    var num = numberOfBlocks;
+    while (num > 0) {
+        output += s4();
+        if (num > 1) output += "-";
+        num--;
+    }
+
+    if (null === document.getElementById(output)) {
+        randomSafeGuid(numberOfBlocks);
+    } else {
+        return output;
+    }
+}
+module.exports = domSafeRandomGuid;
